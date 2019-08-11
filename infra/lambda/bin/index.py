567384360866f20b588aacb2d85243acd3e79758
joeduffy <joe@pulumi.com>
2019-08-11 14:23:29 -0700

    Update lambda build files

--- infra/lambda/bin/index.py
+++ infra/lambda/bin/index.py
@@ -5,6 +5,7 @@ import tarfile
 import tempfile
 import shutil
 import subprocess
+import time
 
 def handler(event, context):
     print(event)
@@ -19,23 +20,34 @@ def handler(event, context):
     archive_key = event['ArchiveKey']
     object_acl = event.get('ObjectAcl')
 
-    # Copy the archive tgz from the bucket to the local disk.
-    s3 = boto3.resource('s3')
-    tmp_archive = tempfile.mktemp(suffix='.tgz')
-    print('| Downloading S3 archive {}/{} to {}...'.format(bucket, archive_key, tmp_archive))
-    s3.meta.client.download_file(bucket, archive_key, tmp_archive)
-    print('| Done.')
+    # Perform the desired S3 action.
+    if action in [ 'Create', 'Update' ]:
+        s3 = boto3.resource('s3')
 
-    try:
-        # Now uncompress the entire archive.
-        tmp_archive_dir = tempfile.mkdtemp()
-        print('| Decompressing archive to {}...'.format(tmp_archive_dir))
-        tarfile.open(tmp_archive).extractall(path=tmp_archive_dir)
+        # Ensure we can access the bucket and object. The IAM attachment is eventually consistent, so there
+        # might be a delay... wait for up to a minute before giving up.
+        for i in range(60):
+            try:
+                s3.head_object(Bucket=bucket, Key=archive_key)
+            except:
+                time.sleep(1)
+                continue
+            break
+
+        # Copy the archive tgz from the bucket to the local disk.
+        tmp_archive = tempfile.mktemp(suffix='.tgz')
+        print('| Downloading S3 archive {}/{} to {}...'.format(bucket, archive_key, tmp_archive))
+        s3.meta.client.download_file(bucket, archive_key, tmp_archive)
         print('| Done.')
 
         try:
-            # Perform the desired S3 action.
-            if action in [ 'Create', 'Update' ]:
+            # Now uncompress the entire archive.
+            tmp_archive_dir = tempfile.mkdtemp()
+            print('| Decompressing archive to {}...'.format(tmp_archive_dir))
+            tarfile.open(tmp_archive).extractall(path=tmp_archive_dir)
+            print('| Done.')
+
+            try:
                 # Run an "AWS S3 sync" command to efficiently decompress the contents. Note that because
                 # we pass the --delete option, the archive itself will also be removed automatically.
                 print('| Running AWS CLI to sync to {}...'.format(bucket))
@@ -45,15 +57,16 @@ def handler(event, context):
                     sync_args.append('--acl')
                     sync_args.append(object_acl)
                 aws(*sync_args)
-            else:
-                # Recursively delete the entire bucket so that it isn't blocked from being deleted itself.
-                print('| Running AWS CLI to delete entire bucket {} ...'.format(bucket))
-                aws('s3', 'rm', bucket, '--recursive')
-            print('| Done.')
+            finally:
+                shutil.rmtree(tmp_archive_dir)
         finally:
-            shutil.rmtree(tmp_archive_dir)
-    finally:
-        os.remove(tmp_archive)
+            os.remove(tmp_archive)
+    else:
+        # Recursively delete the entire bucket so that it isn't blocked from being deleted itself.
+        print('| Running AWS CLI to delete entire bucket {} ...'.format(bucket))
+        aws('s3', 'rm', bucket, '--recursive')
+
+    print('| Done.')
 
 def aws(*args):
     print('AWS: {}'.format(' '.join(args)))
